9a1dbfc248d6504f3e9745795fb42dac60133a93
Smael Nicolas <nico.smael@gmail.com>
2023-07-22 20:00:42 -0300

    init app on port

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const app = express();
+const PORT = process.env.port || 3001;
+
+const routerUsers = require("./routers/users/index.js");
+
+app.use("/api/users", routerUsers);
+
+// HOME
+app.get("/", (request, response) => {
+	response.send("Users api");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:3001`);
+});
