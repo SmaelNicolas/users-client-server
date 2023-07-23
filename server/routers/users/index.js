a400c330f72d237af4945f06ade00e7748e11511
Smael Nicolas <nico.smael@gmail.com>
2023-07-22 21:11:44 -0300

    get value by user

--- server/routers/users/index.js
+++ server/routers/users/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { users } = require("../../data/users");
 
+const userNotFound = "User not found";
 const routerUsers = express.Router();
 
 routerUsers.use(express.json());
@@ -13,7 +14,16 @@ routerUsers.get("/", (req, resp) => {
 routerUsers.get("/:id", (req, resp) => {
 	const id = req.params.id;
 	const userFind = users.find((user) => user.id === id);
-	userFind ? resp.send(userFind) : resp.status(404).send("User not found");
+	userFind ? resp.send(userFind) : resp.status(404).send(userNotFound);
+});
+
+routerUsers.get("/:id/:value", (req, res) => {
+	const { id, value } = req.params;
+	const userFind = users.find((user) => user.id === id);
+	!userFind && res.status(404).send(userNotFound);
+	userFind[value]
+		? res.send(userFind[value])
+		: res.status(404).send(`Value not found for user ${id}`);
 });
 
 module.exports = routerUsers;
