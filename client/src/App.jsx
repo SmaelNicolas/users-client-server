208f427794963ad6bbc8056b60cb3f2a81c2da02
Smael Nicolas <nico.smael@gmail.com>
2023-07-27 21:04:50 -0300

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,21 @@
-export const App = () => <h1> INIT</h1>;
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Users } from "./components/Users";
+import { UsersEdit } from "./components/UsersEdit";
+import { UsersProfile } from "./components/UsersProfile";
+
+export const App = () => {
+	return (
+		<div className="container mx-auto max-w-[1800px] min-h-screen">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/users" element={<Users />} />
+				<Route path="/users/:id" element={<UsersProfile />} />
+				<Route path="/users/edit/:id" element={<UsersEdit />} />
+				<Route path="/works" element={<Home />} />
+				<Route path="/about" element={<Home />} />
+				<Route path="/contact" element={<Home />} />
+			</Routes>
+		</div>
+	);
+};
